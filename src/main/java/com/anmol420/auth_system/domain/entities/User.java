6a4a5e1c0b623f30ea3b333741b52342e5d53f20
anmol420 <anmolanand2k15@gmail.com>
2025-06-11 16:08:49 +0530

    Added User Entity

---
+++ src/main/java/com/anmol420/auth_system/domain/entities/User.java
@@ -0,0 +1,27 @@
+package com.anmol420.auth_system.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
