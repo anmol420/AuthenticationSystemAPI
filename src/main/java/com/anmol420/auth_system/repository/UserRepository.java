525d7b4f5884a24b98e2838ef5b735ec96781f5a
anmol420 <anmolanand2k15@gmail.com>
2025-06-11 16:25:51 +0530

    Added Repository

---
+++ src/main/java/com/anmol420/auth_system/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.anmol420.auth_system.repository;
+
+import com.anmol420.auth_system.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+
+}
